607692fbe664f55684bb344c20cbe890424d5953
mdmansooralam <mdmansooralam@outlook.in>
2025-03-26 16:45:16 +0530

    banner section complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,9 @@ toggler.addEventListener('click', function(){
     // console.log('clicked')
     toggler.classList.toggle('fa-bars')
     toggler.classList.toggle('fa-xmark')
-
     menu.classList.toggle('menu-active')
     menu.classList.toggle('lg:-right-full')
+    document.body.classList.toggle('scroll-disable')
 
 })
 
